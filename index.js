8ff5beec312249fcb8b4defbb202893d7bb8a679
Christopher Regner <dev@chrisregner.com>
2018-10-10 11:49:00 +0800

    Add venues per suburb count; add logging

--- index.js
+++ index.js
@@ -34,14 +34,17 @@ if (!fs.existsSync(OUTPUT_DIR))
 
 const timestamp = +new Date()
 const requestsFn = []
-const suburbIds = []
+const venuesCountBySuburb = {}
 let isStart = true
 
 let errors$
+let output = []
 const input$ = fs.createReadStream(`${INPUT_DIR}/input.json`)
 const output$ = fs.createWriteStream(`${OUTPUT_DIR}/output-${timestamp}.json`)
 
-output$.write('[')
+output.push('[')
+
+console.log('Reading input file...')
 
 oboe(input$)
   .node('{id lat lng}', ({ id, lat, lng, timezone }) => {
@@ -51,33 +54,44 @@ oboe(input$)
           const city = timezoneToCity(timezone)
           const entryId = `${city}/${suburb}`
 
-          if (suburbIds.includes(entryId))
+          if (venuesCountBySuburb[entryId]) {
+            venuesCountBySuburb[entryId]++
             return
+          }
 
-          suburbIds.push(entryId)
+          venuesCountBySuburb[entryId] = 1
 
           const dataKey = error ? 'error' : 'suburb'
 
-          if (isStart)
+          // Add comma on previous object as necessary
+          if (isStart){
             isStart = false
-          else
-            output$.write(`,`)
+          } else {
+            const lastIndex = output.length - 1
+            output[lastIndex] = output[lastIndex] + ','
+          }
 
-          output$.write(
-            '\n' +
-            '  {\n' +
-            `    "lat": ${lat},\n` +
-            `    "lng": ${lng},\n` +
-            `    "city": "${city}",\n` +
-            `    "${dataKey}": "${error || suburb}"\n` +
+          output = output.concat([
+            '  {',
+            `    "lat": ${lat},`,
+            `    "lng": ${lng},`,
+            `    "city": "${city}",`,
+            `    "${dataKey}": "${error || suburb}",`, // keep comma, venues_count will go after
             '  }'
-          )
+          ])
         })
         .catch(console.error)
     )
   })
   .done(() => {
+    let batchNo = 1
+    const totalBatchCount = Math.ceil(requestsFn.length / REQUEST_COUNT_PER_BATCH)
+    console.log('Requesting data from Google Geocoding API...')
+
     const requestByBatchLoop = () => {
+      console.log(`Requesting batch ${batchNo} of ${totalBatchCount}...`)
+      batchNo++
+
       const batchedRequestsFn = requestsFn.splice(0, REQUEST_COUNT_PER_BATCH)
       const batchedRequestsP = batchedRequestsFn.map(requestFn => requestFn())
 
@@ -88,14 +102,34 @@ oboe(input$)
             return
           }
 
-          output$.write('\n]')
+          console.log(`Requests finished!`)
+
+          output.push(']')
+
+          Object.entries(venuesCountBySuburb).forEach(([id, count]) => {
+            const [city, suburb] = id.split('/')
+            const cityIndex = output.findIndex((cityLine, i) => {
+              const suburbLine = output[i + 1]
+              const isSuburbCorrect = suburbLine.includes(`"suburb": "${suburb}"`)
+              const isCityCorrect = isSuburbCorrect && cityLine.includes(`"city": "${city}"`)
+
+              return isSuburbCorrect && isCityCorrect
+            })
+
+            output.splice(cityIndex + 2, 0, `    "venues_count": "${count}"`);
+          })
+
+          output = output.join('\n')
+          output$.write(output)
           output$.end()
 
           if (errors$)
             errors$.end()
 
+          const entries = Object.keys(venuesCountBySuburb).length
+
           console.timeEnd('fill-up-suburbs-data')
-          console.log(`Created ${suburbIds.length} entries`)
+          console.log(`Created ${entries} entries`)
         })
     }
 
@@ -107,7 +141,7 @@ const requestSuburb = ({ id, lat, lng }) =>
   get(GEOCODE_API, {
     params: {
       key: GEOCODE_KEY,
-      latlng: `${lat},${lng}`
+      latlng: `${lat},${lng}`,
     }
   })
     .then(pipe(
@@ -138,7 +172,7 @@ const requestSuburb = ({ id, lat, lng }) =>
       if (!errors$)
         errors$ = fs.createWriteStream(`${OUTPUT_DIR}/errors-${timestamp}.txt`)
 
-      errors$.write(`${id}: ${error}\n`)
+      errors$.write(`${id}: ${error}`)
       return { error }
     })
 
